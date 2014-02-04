cb0f4f866c0abb9fd3ea21e0860e4e3bd8f2b7c3
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-02-04 16:35:45 +0530

    Moving to cdn based bootstrap and font awesome

--- functions.php
+++ functions.php
@@ -95,16 +95,13 @@ add_action( 'widgets_init', 'developr_widgets_init' );
  */
 function developr_scripts() {
     wp_enqueue_style( 'open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,300,700');
-    wp_enqueue_style( 'bootstrap', get_template_directory_uri().'/css/bootstrap.min.css' );
-    wp_enqueue_style( 'font-awesome', get_template_directory_uri().'/fonts/font-awesome.min.css' );
+    wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootswatch/3.1.0/yeti/bootstrap.min.css' );
+    wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' );
 	wp_enqueue_style( 'developr-style', get_stylesheet_uri() );
 
-	//wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '20140120', true );
-    wp_enqueue_script( 'bootstrap-affix', get_template_directory_uri() . '/js/jquery.affix.js', array('jquery'), '20140120', true );
+	wp_enqueue_script( 'bootstrap-affix', get_template_directory_uri() . '/js/jquery.affix.js', array('jquery'), '20140120', true );
     wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ),'', true ); 
  
-	//wp_enqueue_script( 'developr-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
