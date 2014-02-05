846fecd95e6366315888fb122a11ceed9c22b135
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-02-05 23:06:19 +0530

    Removing border from single post and pages

--- content.php
+++ content.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('blogroll'); ?>>
 	<header class="entry-header">
 		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php if ( is_sticky() ) echo'<span class="thumb-icon"><i class="fa fa-star"></i> </span>'; ?><?php the_title(); ?></a></h2>	
 	</header><!-- .entry-header -->
