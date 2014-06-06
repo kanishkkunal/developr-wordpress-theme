f6c7c4214f8f153e75017b3eb5d74734b50af2c9
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-06-06 20:11:08 +0530

    Changing search tag

--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="form-group">
 		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'developr' ); ?></span>
-        <input type="text" class="search form-control" name="s" onblur="if(this.value=='')this.value='<?php _e('To search type and hit enter','developr'); ?>';" onfocus="if(this.value=='<?php _e('To search type and hit enter','anew'); ?>')this.value='';" value="<?php _e('To search type and hit enter','developr'); ?>" />
+        <input type="text" class="search form-control" name="s" onblur="if(this.value=='')this.value='<?php _e('To search type and hit enter','developr'); ?>';" onfocus="if(this.value=='<?php _e('To search type and hit enter','developr'); ?>')this.value='';" value="<?php _e('To search type and hit enter','developr'); ?>" />
 	</div>
 </form>
